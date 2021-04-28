cdab0f5d34db3672ade1b42cf7f57f53e52f9608
Nejc Lampič <nejc.lampic@gmail.com>
2021-04-28 16:15:39 +0200

    final v

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import {motion} from 'framer-motion';
+import {useLocation} from 'react-router-dom';
+
 
 const Nav = ()=>{
+    const {pathname} = useLocation();
     return(
         <StyledNav>
             <h1><Link id="Logo" to="/">Capture</Link></h1>
                 <ul>
                     <li>
                         <Link to="/">1. About Us</Link>
+                        <Line 
+                        transition={{duration:0.5}} 
+                        initial={{width:"0%"}}
+                        animate={{width:pathname=== "/" ? '50%':"0%"}}     
+                        />
                     </li>
                     <li>
                         <Link to="/work"> 2. Our work</Link>
+                        <Line 
+                        transition={{duration:0.5}} 
+                        initial={{width:"0%"}}
+                        animate={{width:pathname=== "/work" ? '50%':"0%"}}     
+                        />
                     </li>
                     <li>
                         <Link to="/contact">3. Contact Us</Link>
+                        <Line 
+                        transition={{duration:0.5}} 
+                        initial={{width:"0%"}}
+                        animate={{width:pathname=== "/contact" ? '50%':"0%"}}     
+                        />
                     </li>
                 </ul>
        
@@ -68,6 +87,18 @@ const StyledNav = styled.nav`
     }
 
 `
+const Line = styled(motion.div)`
+    height:0.3rem;
+    background-color:#23dd9987;
+    width:0%;
+    position:absolute;
+    bottom:-80%;
+    left:60%;
+@media(max-width:1300px){
 
+    left:0%;
+}
+
+`
 
 export default Nav; 
